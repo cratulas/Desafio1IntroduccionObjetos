129ccc12b4e420b560c2114929cf88b37b552706
Mauricio Mora <mauricios.mora@gmail.com>
2021-04-15 16:59:26 -0400

    Listo

---
+++ carta.rb
@@ -0,0 +1,29 @@
+# Ejercicio 1 
+
+class Carta
+
+    # numero (1 al 13) y pinta ( C, D, E, T)
+    attr_accessor :numero, :pinta #GETTERS
+
+    def initialize(numero, pinta)
+
+        pinta.upcase!
+        pintas = ['C','D','E','T']
+
+
+        if (numero >= 1 && numero <= 13) && pintas.include?(pinta)
+            @numero = numero
+            @pinta = pinta
+        else
+            raise ArgumentError.new("falta el argumento")
+
+        end
+    end
+end
+
+pintas = ['C','D','E','T']
+
+
+cartas = [c1 = Carta.new(rand(1..13), pintas.sample(1).join),c2 = Carta.new(rand(1..13), pintas.sample(1).join),c3 = Carta.new(rand(1..13), pintas.sample(1).join),c4 = Carta.new(rand(1..13), pintas.sample(1).join),c5 = Carta.new(rand(1..13), pintas.sample(1).join)]
+
+print cartas
---
+++ cuenta_bancaria.rb
@@ -0,0 +1,48 @@
+# Ejercicio 2
+
+
+# 1. Crear la clase con el nombre CuentaBancaria
+class CuentaBancaria
+
+    # Los métodos getter y setter para el atributo nombre_de_usuario
+    attr_accessor :nombre_de_usuario
+    attr_reader :numero_de_cuenta
+    attr_reader :vip
+
+
+
+    # Crear el constructor que reciba el nombre_usuario y numero_de_cuenta y lo asigne a los atributos
+    #Agregar un tercer parámetro opcional al constructor que permita establecer si una cuenta es VIP (1pto), este valor puede ser 1 o 0. Por defecto será 0.
+
+    def initialize(nombre_usuario,numero_de_cuenta,vip = 0)
+        
+
+        # Levantar una excepción del tipo RangeError si el atributo numero_de_cuenta tiene un número de dígitos distinto a 8 (se puede ocupar el método .digits para obtener los dígitos y .count para contarlos)
+        if  numero_de_cuenta.digits.count != 8
+            raise RangeError.new('El digito ingresado debe contener un largo de 8')
+        elsif vip != 0 && vip != 1
+            raise RangeError.new('El digito a ingresar para definir si es una cuenta es vip es 1 o 0')
+        else 
+            @nombre_usuario = nombre_usuario
+            @numero_de_cuenta = numero_de_cuenta
+            @vip = vip
+        end
+    end
+
+    #Crear un método llamado numero_de_cuenta que devuelva con el número de cuenta con un prefijo '1-' si es vip y '0-' si no lo es. Ejemplo: si la cuenta es VIP y el número 00112233, el método debería devolver '1-00112233'
+
+    def EsVip
+
+        stringVip = "1-"
+        stringNoVip = "0-"
+        cuenta = self.numero_de_cuenta.to_s
+        stringFinal = ""
+        if self.vip == 0
+            stringFinal = stringFinal +stringNoVip+cuenta
+        elsif self.vip == 1
+            stringFinal = stringFinal +stringVip+cuenta
+        end
+        stringFinal
+    end
+
+end
